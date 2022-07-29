e64a3d8e997a536d04a51df75ca7bed6b9520310
manoj <manojnjaya@gmail.com>
2022-07-29 16:47:24 +0530

    create user sign up

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,50 @@
+const User=require('../models/user')
 module.exports.profile=function(req,res){
-    return res.end('<h1>User Profile</h1>');
+    return res.render('user_profile',{
+        title:" User Profile"
+    })
+
+}
+//render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_signup',{
+        title:"Codeial | Sign Up"
+    })
+}
+//render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_signin',{
+        title:"Codeial | Sign In"
+    })
+}
+
+//get the sign up data
+
+module.exports.create=function(req,res){
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+
+//get the sign in data
+
+module.exports.createSession=function(req,res){
+
 }
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express=require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
 const port=8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//app.use(express.urlencoded); this is depricated
+app.use(cookieParser());
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 
@@ -8,7 +24,8 @@ const port=8000;
 
 //use express router
 app.use('/',require("./routes"));
-
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server:${err}`);
