c7fce8a44a1cac5fb9a2dfe0b5912560b8f55367
manoj <manojnjaya@gmail.com>
2022-07-29 11:43:06 +0530

    added routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const express=require('express');
 const homeController=require("../controllers/home_controller");
 
  router.get('/',homeController.home);
+ router.use('/users',require('./users'));
  module.exports=router;
