7bc9b1fcb510e7b67accf95bbf6047aa4da4f6d5
Manoj <manojnjaya@gmail.com>
2022-08-04 14:56:27 +0530

    created posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,21 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
+   // console.log(req.cookies);
+
+ // Post.find({},function(err,posts){
+ //   return res.render('home',{
+ //       title:"Codeial |Home",
+ //       posts:posts
+//  });
+//});
+
+//populate the user of each post
+   Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home',{
-        title:"Home"
-    })
+      title:"Codeial |Home",
+      posts:posts
+});
+   });
+    
 }
