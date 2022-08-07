635d2a3195f821f247b3a43ccc5942562fae0934
Manoj <manojnjaya@gmail.com>
2022-08-07 22:55:50 +0530

    updated user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post=require('../models/post');
-
+const User =require('../models/user');
 module.exports.home=function(req,res){
    // console.log(req.cookies);
 
@@ -20,10 +20,15 @@ module.exports.home=function(req,res){
       }
    })
    .exec(function(err,posts){
-    return res.render('home',{
+User.find({},function(err,users){
+   return res.render('home',{
       title:"Codeial |Home",
-      posts:posts
+      posts:posts,
+      all_users:users
 });
+});
+
+   
    });
     
 }
